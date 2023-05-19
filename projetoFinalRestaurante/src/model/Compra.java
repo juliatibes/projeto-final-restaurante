2579024e9818630872c3e634e1aa3bebccf18d3c
IsacGuedes <witcherthe0869@gmail.com>
2023-05-19 19:31:21 -0300

    Foi criada a classe estoque. Foi modificado a classe compra.

--- projetoFinalRestaurante/src/model/Compra.java
+++ projetoFinalRestaurante/src/model/Compra.java
@@ -7,89 +7,10 @@ public class Compra {
 
     private Integer id;
     private String dataCompra; //mudar para LOCALDATE perguntar para o teacher
-    private String dataValidade; //mudar para LOCALDATE perguntar para o teacher JOPTION
-    private String nomeProduto;
+    private Produto produto;
     private Double quantidade;
     private UnidadeMedidaEnum unidadeMedida;
     private BigDecimal valorCompra;
 
-    public Compra(Integer id, String dataCompra, String dataValidade, String nomeProduto,
-                  Double quantidade, UnidadeMedidaEnum unidadeMedida, BigDecimal valorCompra) {
-        this.id = id;
-        this.dataCompra = dataCompra;
-        this.dataValidade = dataValidade;
-        this.nomeProduto = nomeProduto;
-        this.quantidade = quantidade;
-        this.unidadeMedida = unidadeMedida;
-        this.valorCompra = valorCompra;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getDataCompra() {
-        return dataCompra;
-    }
-
-    public void setDataCompra(String dataCompra) {
-        this.dataCompra = dataCompra;
-    }
-
-    public String getDataValidade() {
-        return dataValidade;
-    }
-
-    public void setDataValidade(String dataValidade) {
-        this.dataValidade = dataValidade;
-    }
-
-    public String getNomeProduto() {
-        return nomeProduto;
-    }
-
-    public void setNomeProduto(String nomeProduto) {
-        this.nomeProduto = nomeProduto;
-    }
-
-    public Double getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(Double quantidade) {
-        this.quantidade = quantidade;
-    }
-
-    public UnidadeMedidaEnum getUnidadeMedida() {
-        return unidadeMedida;
-    }
-
-    public void setUnidadeMedida(UnidadeMedidaEnum unidadeMedida) {
-        this.unidadeMedida = unidadeMedida;
-    }
-
-    public BigDecimal getValorCompra() {
-        return valorCompra;
-    }
-
-    public void setValorCompra(BigDecimal valorCompra) {
-        this.valorCompra = valorCompra;
-    }
-
-    @Override
-    public String toString() {
-        return "Compra{" +
-                "id=" + id +
-                ", dataCompra='" + dataCompra + '\'' +
-                ", dataValidade='" + dataValidade + '\'' +
-                ", nomeProduto='" + nomeProduto + '\'' +
-                ", quantidade=" + quantidade +
-                ", unidadeMedida=" + unidadeMedida +
-                ", valorCompra=" + valorCompra +
-                '}';
-    }
 }
+
---
+++ projetoFinalRestaurante/src/model/Estoque.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.math.BigDecimal;
+
+public class Estoque {
+    private Integer id;
+    private Compra compra;
+    private Double quantidade;
+    private UnidadeMedidaEnum unidadeMedida;
+    private BigDecimal valorCusto;
+    private StatusEnum status;
+
+
+}
