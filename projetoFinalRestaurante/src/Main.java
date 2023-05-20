6a3fe7697160feefc2ac1f6cf11f12c5d648822b
GustavoAlbonico <gustavoalbonicogoncalves@hotmail.com>
2023-05-19 21:17:21 -0300

    alterando as informações do produtoDAO com JOptionPane para listbox e no main tambem.

--- projetoFinalRestaurante/src/Main.java
+++ projetoFinalRestaurante/src/Main.java
@@ -97,4 +97,5 @@ public class Main {
         CompraDAO.salvarNovaCompra(compra);
         chamaMenuPrincipal();
 
+
 }
--- projetoFinalRestaurante/src/repository/ProdutoDAO.java
+++ projetoFinalRestaurante/src/repository/ProdutoDAO.java
@@ -34,7 +34,5 @@ public class ProdutoDAO {
         return produtosNomes.toArray();
     }
 
-
-
     }
 
